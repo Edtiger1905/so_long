0619980a2fe0ae4ba92ccac943da3feb067f0f91
Eduardo Andrei Pandele Tecla <epandele@e4r13p3.42roma.it>
2026-02-04 14:16:49 +0100

    pushcluster

--- src/player_movement.c
+++ src/player_movement.c
@@ -1,20 +1,52 @@
 
 #include "../includes/so_long.h"
 
+void check_coin(t_map *game, int x, int y)
+{
+    if(game->matrix[x][y] == 'C')
+    {
+        game->coin--;
+        game->matrix[x][y] = '0';
+    }
+}
 
-int key_hook(int keycode, t_map *game)
+int check_exit(t_map *game, t_map *map, int x, int y)
+{
+    if(game->coin == 0)
+    {
+        exit_game(game, map->mlx);
+        return (0);
+    }
+    return (1);
+}
+void move_player(t_map *game, int p_x, int p_y)
+{
+    int new_x;
+    int new_y;
+
+    new_x = pos_x + p_x;
+    new_y = pos_y + p_y;
+
+    if(new_x >= 0 && new_x < game->rows && new_y >= 0 && new_y < game->column 
+        && game->matrix[new_x][new_y] != '1')
+    {
+        check_coin(game, new_x, new_y);
+    }
+}
+
+int key_hook(int keycode, t_map *game, t_map *map)
 {
     if(keycode ==  65363 || keycode == 100)
-        mov_pg;
+        mov_player(game, 1, 0);
     else if(keycode == 65361 || keycode == 97)
-        mov_pg;
-    else if(keycode == 65362 || keycode == 119 )
-        move_pg;
+        mov_player(game, -1, 0);
+    else if(keycode == 65362 || keycode == 119)
+        move_player(game, 0 -1);
     else if(keycode == 65364 || keycode == 115)
-        move_pg;
+        move_player(game, 0, 1);
     else if(keycode == 65307)
     {   
-        exit_game(game, );
+        exit_game(game, map->mlx);
         exit(0);
     }
     return (0);
